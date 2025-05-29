fda01969522ee7b2f20da39288cbf4cf7b291462
Aaryan Makan <st10259834@vcconnect.edu.za>
2025-05-29 20:21:32 +0200

    ice task 3 push

---
+++ EventSystem/Controllers/EventController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EventSystem.Data;
+using EventSystem.Models;
+
+// Code adapted from Microsoft Learn ASP.NET Core Tutorial:
+// "Add a controller to an ASP.NET Core MVC app"
+// Source: https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/adding-controller?view=aspnetcore-9.0&tabs=visual-studio
+// Retrieved on: April 7, 2025
+
+
+// Code adapted from W3Schools tutorial:
+// "How TO - Top Navigation"
+// Source: https://www.w3schools.com/howto/howto_js_topnav.asp
+// Retrieved on: April 7, 2025
+
+
+// Code adapted from W3Schools C# Tutorial
+// Source: https://www.w3schools.com/cs/index.php
+// Retrieved on: April 7, 2025
+
+
+
+namespace EventSystem.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly EventEaseDbContext _context;
+
+        public EventController(EventEaseDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Event
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Events.ToListAsync());
+        }
+
+        // GET: Event/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events
+                .FirstOrDefaultAsync(m => m.EventId == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        // GET: Event/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Event/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EventId,EventName,EventDate,Description")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@event);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@event);
+        }
+
+        // GET: Event/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events.FindAsync(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+            return View(@event);
+        }
+
+        // POST: Event/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EventId,EventName,EventDate,Description")] Event @event)
+        {
+            if (id != @event.EventId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@event);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventExists(@event.EventId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@event);
+        }
+
+        // GET: Event/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events
+                .FirstOrDefaultAsync(m => m.EventId == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        // POST: Event/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @event = await _context.Events.FindAsync(id);
+            if (@event != null)
+            {
+                _context.Events.Remove(@event);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventExists(int id)
+        {
+            return _context.Events.Any(e => e.EventId == id);
+        }
+    }
+}
---
+++ EventSystem/Data/EventEaseDbContext.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using EventSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventSystem.Data;
+
+public partial class EventEaseDbContext : DbContext
+{
+    public EventEaseDbContext()
+    {
+    }
+
+    public EventEaseDbContext(DbContextOptions<EventEaseDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Booking> Bookings { get; set; }
+
+    public virtual DbSet<Event> Events { get; set; }
+
+    public virtual DbSet<Venue> Venues { get; set; }
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Booking>(entity =>
+        {
+            entity.HasKey(e => e.BookingId).HasName("PK__Booking__73951ACD10DFC753");
+
+            entity.ToTable("Booking");
+
+            entity.Property(e => e.BookingId).HasColumnName("BookingID");
+            entity.Property(e => e.EventId).HasColumnName("EventID");
+            entity.Property(e => e.VenueId).HasColumnName("VenueID");
+
+            entity.HasOne(d => d.Event).WithMany(p => p.Bookings)
+                .HasForeignKey(d => d.EventId)
+                .HasConstraintName("FK__Booking__EventID__3C69FB99");
+
+            entity.HasOne(d => d.Venue).WithMany(p => p.Bookings)
+                .HasForeignKey(d => d.VenueId)
+                .HasConstraintName("FK__Booking__VenueID__3B75D760");
+        });
+
+        modelBuilder.Entity<Event>(entity =>
+        {
+            entity.HasKey(e => e.EventId).HasName("PK__Event__7944C8706394B9E0");
+
+            entity.ToTable("Event");
+
+            entity.Property(e => e.EventId).HasColumnName("EventID");
+            entity.Property(e => e.Description)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.EventName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Venue>(entity =>
+        {
+            entity.HasKey(e => e.VenueId).HasName("PK__Venue__3C57E5D2D4CA16E9");
+
+            entity.ToTable("Venue");
+
+            entity.Property(e => e.VenueId).HasColumnName("VenueID");
+            entity.Property(e => e.ImageUrl)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Location)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.VenueName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
